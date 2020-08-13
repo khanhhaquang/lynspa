8e3653b68c748f2f64a534806255869fdf54ddec
Ken Ha <khanhha@nvg.vn>
2020-08-13 22:52:45 +0700

    init

--- src/components/seo.js
+++ src/components/seo.js
@@ -52,19 +52,19 @@ function SEO({ description, lang, meta, title }) {
           content: `website`,
         },
         {
-          name: `twitter:card`,
+          name: `facebook:card`,
           content: `summary`,
         },
         {
-          name: `twitter:creator`,
+          name: `facebook:creator`,
           content: site.siteMetadata.author,
         },
         {
-          name: `twitter:title`,
+          name: `facebook:title`,
           content: title,
         },
         {
-          name: `twitter:description`,
+          name: `facebook:description`,
           content: metaDescription,
         },
       ].concat(meta)}
