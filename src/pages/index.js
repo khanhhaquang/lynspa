c935e8d09e8d287f9e2ea078c62898ec94d681ed
Ken Ha <khanhha@nvg.vn>
2020-08-14 02:13:27 +0700

    update background color

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,26 +4,25 @@ import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import BackgroundImage from "../components/backgroundImage"
 
 const Background = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
-
-  > div {
-    min-height: 100vh;
-  }
+  min-height: 100vh;
+  background: linear-gradient(
+    0deg,
+    rgba(0, 0, 0, 1) 0%,
+    rgba(80, 43, 43, 1) 100%
+  );
 `
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Trang chủ" />
-      <Background>
-        <BackgroundImage />
-      </Background>
+      <Background></Background>
     </Layout>
   )
 }
