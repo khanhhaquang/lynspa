c6041f85c2d9d05a4ffc57140916ffcc284be985
Ken Ha <khanhha@nvg.vn>
2020-08-16 19:08:11 +0700

    add Menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,15 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import LogoImage from "./logoImage"
+import logo from "../images/logo.png"
 import styled from "styled-components"
-
+import { IoIosMenu, IoMdCloseCircleOutline } from "react-icons/io"
 const HeaderWrapper = styled.header`
   width: 100%;
   display: flex;
   justify-content: center;
-  position: fixed;
+  position: relative;
   top: 0;
   left: 0;
   z-index: 999;
@@ -18,12 +19,105 @@ const HeaderWrapper = styled.header`
   }
 `
 
+const MenuToggle = styled.div`
+  position: absolute;
+  left: 16px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+`
+
+const Menu = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 999;
+  transition: opacity 0.6s cubic-bezier(0.23, 1, 0.32, 1) 0s;
+  background-color: rgba(0, 0, 0, 0.08);
+  opacity: ${props => (props.isOpen ? 1 : 0)};
+  pointer-events: ${props => (props.isOpen ? "all" : "none")};
+
+  .inner {
+    width: 240px;
+    min-height: 100%;
+    overflow-y: scroll;
+    position: fixed;
+    top: 0;
+    left: 0;
+    box-shadow: 4px 0px 8px rgba(38, 42, 51, 0.15);
+    background-color: #fff;
+    padding: 16px;
+    transition: transform 0.6s cubic-bezier(0.23, 1, 0.32, 1) 0s,
+      opacity 0.6s cubic-bezier(0.23, 1, 0.32, 1) 0s;
+    transform: ${props =>
+      props.isOpen ? "translateX(0)" : "translateX(-240px)"};
+
+    header {
+      img {
+        width: 120px;
+      }
+
+      svg {
+        position: absolute;
+        top: 16px;
+        right: 16px;
+        cursor: pointer;
+      }
+    }
+
+    ul {
+      list-style: none;
+      margin-left: 0;
+
+      li {
+        margin-bottom: 24px;
+
+        a {
+          text-decoration: none;
+          color: #000;
+        }
+      }
+    }
+  }
+`
+
 const Header = ({ siteTitle }) => {
+  const [isMenuOpen, toggleMenu] = useState(false)
+
   return (
     <HeaderWrapper>
       <Link to="/">
         <LogoImage />
       </Link>
+      <MenuToggle onClick={() => toggleMenu(true)}>
+        <IoIosMenu color="#fff" size="24px" />
+      </MenuToggle>
+
+      <Menu isOpen={isMenuOpen} onClick={() => toggleMenu(false)}>
+        <div className="inner" onClick={e => e.stopPropagation()}>
+          <header>
+            <img src={logo}></img>
+            <IoMdCloseCircleOutline
+              size="24px"
+              onClick={() => toggleMenu(false)}
+            />
+          </header>
+
+          <ul>
+            <li>
+              <Link to="/">Trang chủ</Link>
+            </li>
+            <li>Thẩm mỹ không phẩu thuật</li>
+            <li>Làm hồng vĩnh viễn</li>
+            <li>Các dịch vụ khác</li>
+            <li>
+              <Link to="/contact">Liên hệ</Link>
+            </li>
+          </ul>
+        </div>
+      </Menu>
     </HeaderWrapper>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,58 @@
 import React from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
-
+import { IoIosCall, IoLogoFacebook, IoIosMap } from "react-icons/io"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Background = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  min-height: 100vh;
-  background: linear-gradient(
-    0deg,
-    rgba(0, 0, 0, 1) 0%,
-    rgba(80, 43, 43, 1) 100%
-  );
+const Main = styled.div`
+  z-index: 1;
+  color: #fff;
+  margin-top: 40px;
+
+  ul {
+    list-style: none;
+
+    li {
+      margin-bottom: 16px;
+
+      svg {
+        width: 24px;
+        height: 24px;
+        margin-right: 16px;
+        vertical-align: top;
+      }
+
+      a {
+        color: #fff;
+      }
+    }
+  }
 `
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Trang chủ" />
-      <Background></Background>
+      <Main>
+        <h1>Liên hệ</h1>
+        <ul>
+          <li>
+            <IoIosCall />
+            <span>0923456036</span>
+          </li>
+          <li>
+            <IoLogoFacebook />
+            <a href="https://www.facebook.com/LYN.Spa.TPHCM/" target="_blank">
+              Facebook LynSpa
+            </a>
+          </li>
+          <li>
+            <IoIosMap />
+            <span>122 Nguyễn Thái Học, P. Tân Thành, Q. Tân Phú</span>
+          </li>
+        </ul>
+      </Main>
     </Layout>
   )
 }
